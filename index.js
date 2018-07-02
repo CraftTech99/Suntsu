f76ffa71c38e139fa0031130d406b1c1a7f37a7b
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-02 17:32:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ client.on('message', message => {
 if (message.content.startsWith("/help")) {
   message.channel.send("Type: /owner, /fac staff, /fac members");
 } 
-  if (message.content.startsWith("/owner")) {
+if (message.content.startsWith("/owner")) {
     message.channel.send("The Honorable Owner of the Faction Suntsu is Wendiya"); 
   }
   if (message.content.startwith("/fac staff")) {
-    message.channel.send("The Respected staff members of Suntsu are: wendiya, mackaylee, Razztee, BigDaddy2003, EmpereurNr2, HuskyBadger843 aka Vishnu19")
+    message.chanel.send("The Respected staff members of Suntsu are: wendiya, mackaylee, Razztee, BigDaddy2003, EmpereurNr2, HuskyBadger843 aka Vishnu19")
   }
-  is (message.content.startwith("/fac members")) {
+  if (message.content.startwith("/fac members")) {
     message.channel.send("Members of Suntsu are: Chaos_Knight, coelprit, DokenBoy, fanjaki, hellbound, kermitzafrog, Lean123shen, Nekta, Neplosion, Phantom Hyperion, rudra30, samantha, sansfoyer, ShadowFreddy, Sicogina, Tazky, Twitch_manobeast10, Viebe2, Vikram, VINHei, ZKillUchiha1");
          }
 
