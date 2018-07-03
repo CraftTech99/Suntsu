55bf310841cf5288fb6f1d4ee4e89dc28990f643
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-03 13:29:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const prefix = process.env.BOT_PREFIX
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -9,20 +10,20 @@ client.on('ready', () => {
     });
 client.on("message", (message) => {
   
-  if (message.content.startsWith("shelp")) {
+  if (message.content.startsWith(`${prefix}help`)) {
     message.channel.send("```Type the following commands with no uppercase letters:```sowner, sfacstaff, sfacmembers, sinvite");
   }
 
-  if (message.content.startsWith("sowner")) {
+  if (message.content.startsWith(`${prefix}owner`)) {
     message.channel.send("The Honorable Owner of the Faction Suntsu is Wendiya");
   }
-  if (message.content.startsWith("sfacstaff")) {  
+  if (message.content.startsWith(`${prefix}facstaff`)) {  
     message.channel.send("The Respected staff members of Suntsu are: wendiya, mackaylee, Razztee, BigDaddy2003, EmpereurNr2, HuskyBadger843 aka Vishnu19"); 
   }
-   if (message.content.startsWith("sfacmembers")) {
+   if (message.content.startsWith(`${prefix}facmembers`)) {
     message.channel.send("Members of Suntsu are: Chaos_Knight, coelprit, DokenBoy, fanjaki, hellbound, kermitzafrog, Lean123shen, Nekta, Neplosion, Phantom Hyperion, rudra30, samantha, sansfoyer, ShadowFreddy, Sicogina, Tazky, Twitch_manobeast10, Viebe2, Vikram, VINHei, ZKillUchiha1");
   }
-  if (message.content.startsWith("sinvite")) {
+  if (message.content.startsWith(`${prefix}invite`)) {
     message.channel.send("https://discord.gg/5T58THC");
   }
 });
