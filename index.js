44f4f301273da2342c32ebb29236502a16b4dd8a
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-12 20:48:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on("message", (message) => {
     message.channel.send("```The Respected staff members of Suntsu are:`````` wendiya, mackaylee, Razztee, BigDaddy2003, EmpereurNr2, HuskyBadger843 aka Vishnu19```"); 
   }
    if (message.content.startsWith(`${prefix}facmembers`)) {
-    message.channel.send("```Members of Suntsu are:`````` Chaos_Knight, coelprit, DokenBoy, fanjaki, hellbound, kermitzafrog, Lean123shen, Nekta, Neplosion, Phantom Hyperion, rudra30, samantha, sansfoyer, ShadowFreddy, Sicogina, Tazky, Twitch_manobeast10, Viebe2, Vikram, VINHei, ZKillUchiha1```");
+    message.channel.send("```Members of Suntsu are:`````` Chaos_Knight, Jarcle, DokanBoy, fanjaki, hellbound, kermitzafrog, Lean123shen, Nekta, Neplosion, Phantom Hyperion, rudra30, samantha, Yaboku, ShadowFreddy, Sicogina, Tazky, Twitch_manobeast10, Viebe2, Vikram, VINHei, ZKillUchiha1, WoodieTree, Freepurplehaze```");
   }
   if (message.content.startsWith(`${prefix}invite`)) {
     message.channel.send("https://discord.gg/5T58THC");
